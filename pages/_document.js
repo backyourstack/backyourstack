87332a605e46d8ec34d77f584c7d2d3e292111e3
François Hodierne <francois@hodierne.net>
2018-06-14 11:53:02 +0200

    refactor: add global document.css

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
+import '../css/document.css';
+
 export default class MyDocument extends Document {
 
   static async getInitialProps(ctx) {
@@ -11,30 +13,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <style>{`
-          html, body {
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            background: black;
-            font-family: lato, montserratlight, Helvetica, sans-serif;
-            font-size: 14px;
-          }
-          h1 {
-            font-size: 2em;
-          }
-          h2 {
-            font-size: 1.6em;
-          }
-          #__next {
-            background: #f1f1f1;
-            margin: 0 auto;
-            width: 1024px;
-            min-height: 100%;
-          }
-          `}
-          </style>
+          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
