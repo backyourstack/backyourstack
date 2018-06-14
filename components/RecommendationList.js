7486fc233f59f812bd80dd44e306027cd5efd593
François Hodierne <francois@hodierne.net>
2018-06-14 11:49:08 +0200

    refactor: recommendation list

---
+++ components/RecommendationList.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class RecommendationList extends React.Component {
+
+  static propTypes = {
+    recommendations: PropTypes.array.isRequired,
+  }
+
+  render () {
+    const { recommendations } = this.props;
+
+    return (
+      <div>
+        <style jsx>{`
+        .Recommendations {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+        .Recommendation {
+          width: 180px;
+          min-height: 300px;
+          border-radius: 15px;
+          background-color: #ffffff;
+          box-shadow: 0 1px 3px 0 rgba(45, 77, 97, 0.2);
+          margin: 10px;
+          padding: 15px;
+          text-align: center;
+        }
+        .Recommendation .name {
+          height: 50px;
+        }
+        .Recommendation .description {
+          font-size: 12px;
+          color: #333;
+          height: 100px;
+        }
+        .Recommendation .repos {
+          height: 100px;
+          font-size: 12px;
+        }
+        .Recommendation .repos {
+          color: #333;
+        }
+        .Recommendation .repos a {
+          color: #666;
+        }
+        .Recommendation .repos a:hover {
+          color: #333;
+          text-decoration: none;
+        }
+        .back-button {
+
+        }
+        .back-button a {
+          display: block;
+          border-radius: 15px;
+          padding: 10px;
+          text-decoration: none;
+          background: #333;
+          color: white;
+        }
+        .back-button a:hover {
+          background: #666;
+        }
+        `}
+        </style>
+        <div className="Recommendations">
+          {recommendations.filter(r => r.opencollective).map(recommendation => (
+            <div key={recommendation.id} className="Recommendation">
+              <div className="name"><b>{recommendation.opencollective.name}</b></div>
+              <div className="description">{recommendation.opencollective.description}</div>
+              <div className="repos">
+                <strong>Used in</strong>:&nbsp;
+                {recommendation.repos.slice(0, 3).map(repo => (
+                  <span key={repo.id}>
+                    <a href={`https://github.com/${repo.full_name}`}>{repo.full_name}</a>
+                  </span>
+                )).reduce( ( prev, curr ) => [ prev, ', ', curr ] )}
+                {recommendation.repos.length > 3 && ` and ${recommendation.repos.length - 3 } others`}
+              </div>
+              <div className="back-button">
+                <a href={`https://opencollective.com/${recommendation.opencollective.slug}`}>
+                  Back this project
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+}
--- pages/profile.js
+++ pages/profile.js
@@ -21,6 +21,7 @@ import Footer from '../components/Footer';
 
 import DependencyTable from '../components/DependencyTable';
 import RepositoryTable from '../components/RepositoryTable';
+import RecommendationList from '../components/RecommendationList';
 
 export default class Profile extends React.Component {
 
@@ -51,60 +52,6 @@ export default class Profile extends React.Component {
     const { error, profile, repos, dependencies, recommendations, pathname, loggedInUser } = this.props;
     return (
       <div>
-        <style jsx>{`
-        .Recommendations {
-          display: flex;
-          flex-wrap: wrap;
-          justify-content: center;
-        }
-        .Recommendation {
-          width: 180px;
-          min-height: 300px;
-          border-radius: 15px;
-          background-color: #ffffff;
-          box-shadow: 0 1px 3px 0 rgba(45, 77, 97, 0.2);
-          margin: 10px;
-          padding: 15px;
-          text-align: center;
-        }
-        .Recommendation .name {
-          height: 50px;
-        }
-        .Recommendation .description {
-          font-size: 12px;
-          color: #333;
-          height: 100px;
-        }
-        .Recommendation .repos {
-          height: 100px;
-          font-size: 12px;
-        }
-        .Recommendation .repos {
-          color: #333;
-        }
-        .Recommendation .repos a {
-          color: #666;
-        }
-        .Recommendation .repos a:hover {
-          color: #333;
-          text-decoration: none;
-        }
-        .back-button {
-
-        }
-        .back-button a {
-          display: block;
-          border-radius: 15px;
-          padding: 10px;
-          text-decoration: none;
-          background: #333;
-          color: white;
-        }
-        .back-button a:hover {
-          background: #666;
-        }
-        `}
-        </style>
         <Header loggedInUser={loggedInUser} pathname={pathname} />
         <Content>
           {error &&
@@ -120,63 +67,23 @@ export default class Profile extends React.Component {
               <h1>{profile.name}</h1>
               <div>
                 <strong>Github Profile</strong>:&nbsp;
-                <a href={`https://github.com/${profile.login}`}>github.com/{profile.login}</a>
+                <a href={`https://github.com/${profile.login}`}>
+                  {`github.com/${profile.login}`}
+                </a>
                 &nbsp;-&nbsp;
                 <strong>OpenCollective Profile</strong>:&nbsp;
                 <em>unknown</em>
               </div>
+
               <h2>Recommendations</h2>
-              <div className="Recommendations">
-                {recommendations.filter(r => r.opencollective).map(recommendation => (
-                  <div key={recommendation.id} className="Recommendation">
-                    {recommendation.opencollective && (
-                      <>
-                        <div className="name"><b>{recommendation.opencollective.name}</b></div>
-                        <div className="description">{recommendation.opencollective.description}</div>
-                        <div className="repos">
-                          <strong>Used in</strong>:&nbsp;
-                          {recommendation.repos.slice(0, 3).map(repo => (
-                            <span key={repo.id}>
-                              <a href={`https://github.com/${repo.full_name}`}>{repo.full_name}</a>
-                            </span>
-                          )).reduce( ( prev, curr ) => [ prev, ', ', curr ] )}
-                          {recommendation.repos.length > 3 && ` and ${recommendation.repos.length - 3 } others`}
-                        </div>
-                        <div className="back-button">
-                          <a href={`https://opencollective.com/${recommendation.opencollective.slug}`}>
-                            Back this project
-                          </a>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                ))}
-              </div>
+              <RecommendationList recommendations={recommendations} />
 
               <h2>Dependencies</h2>
               <DependencyTable dependencies={dependencies} />
 
               <h2>Repositories</h2>
               <RepositoryTable repositories={repos} />
-              <ul>
-                {repos.map(repo => (
-                  <li key={repo.id}>
-                    <p>
-                      {repo.full_name}<br />
-                      <em>{repo.dependencies.length} dependencies detected</em>
-                    </p>
-                    <div style={{ display: 'none' }}>
-                      {repo.dependencies && (
-                        <ul>
-                          {repo.dependencies.map(dep => (
-                            <li key={dep.name}>{dep.name}</li>
-                          ))}
-                        </ul>
-                      )}
-                    </div>
-                  </li>
-                ))}
-              </ul>
+
             </div>
           }
         </Content>
