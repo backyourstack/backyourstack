9446852e262bae2f28ac4c7b0deb3312feb95ff4
François Hodierne <francois@hodierne.net>
2018-06-14 12:48:03 +0200

    feature: display organizations from authenticated user if any

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
+
+import { getOrgsForUser } from '../lib/github';
 
 import { Link } from '../routes';
 
@@ -9,17 +12,53 @@ import Footer from '../components/Footer';
 
 export default class Index extends React.Component {
 
+  static async getInitialProps ({ req, query }) {
+    const initialProps = {};
+
+    let accessToken;
+    if (req) {
+      accessToken = get(req, 'session.passport.user.accessToken');
+    } else if (typeof window !== 'undefined') {
+      accessToken = get(window, '__NEXT_DATA__.props.pageProps.loggedInUser.accessToken');
+    }
+
+    if (accessToken) {
+      initialProps.loggedInUserOrgs = await getOrgsForUser(accessToken);
+    }
+
+    return initialProps;
+  }
+
   static propTypes = {
     pathname: PropTypes.string,
     loggedInUser: PropTypes.object,
   }
 
   render () {
-    const { pathname, loggedInUser } = this.props;
+    const { pathname, loggedInUser, loggedInUserOrgs } = this.props;
     return (
       <div>
         <Header pathname={pathname} loggedInUser={loggedInUser} />
         <Content>
+          {loggedInUser && (
+            <>
+              <h2>Welcome {loggedInUser.username}</h2>
+              {loggedInUserOrgs &&
+                <>
+                  <p>Your organizations:</p>
+                  <ul>
+                    {loggedInUserOrgs.map(org => (
+                      <li key={org.id}>
+                        <Link route="profile" params={{ id: org.login }}>
+                          <a>{org.login}</a>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              }
+            </>
+          )}
           <h2>Sample Organizations</h2>
           <ul>
             <li><Link route="profile" params={{ id: 'facebook' }}><a>Facebook</a></Link></li>
