055071548ba8b3260157fc6661a95186f4631418
François Hodierne <francois@hodierne.net>
2018-07-06 15:37:15 +0200

    fix: don't focus when navigating to an example

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -65,7 +65,6 @@ export default class SearchForm extends React.Component {
   search = (event, q) => {
     event.preventDefault();
     event.stopPropagation();
-    this.focus();
     this.setState({ q });
     Router.pushRoute('search', { q });
   };
