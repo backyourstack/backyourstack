47c4e9e15a07c90a47c938399710c99e7491b645
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-08-21 17:42:10 -0300

    wip: removing console.logs

--- src/lib/dependencies/dep.js
+++ src/lib/dependencies/dep.js
@@ -9,11 +9,10 @@ import { fetchFileFromRepo } from '../github';
 const _debug = debug('dependencies:dep');
 
 function getDependenciesFromGithubRepo (githubRepo, githubAccessToken) {
-  console.log('lets goooo')
   const cacheKey = `repo_dep_dependencies_${githubRepo.id}`;
-  // if (cache.has(cacheKey)) {
-  //   return cache.get(cacheKey);
-  // }
+  if (cache.has(cacheKey)) {
+    return cache.get(cacheKey);
+  }
   return fetchFileFromRepo(githubRepo, 'Gopkg.lock', githubAccessToken)
     .then(dependenciesStats)
     .catch(err => {
@@ -21,20 +20,15 @@ function getDependenciesFromGithubRepo (githubRepo, githubAccessToken) {
       return [];
     })
     .then(result => {
-      console.log('here we go:')
-      console.dir(result)
       cache.set(cacheKey, result);
       return result;
     });
 }
 
 function dependenciesStats (file) {
-  console.log("file: " + file)
   const data = toml.parse(file)
-  console.dir(data);
   const dependencies = {};
   data.projects.map(proj => proj.name).forEach(name => {
-    console.log("adding " + name)
     dependencies[name] = dependencies[name] || {
       type: 'dep',
       name: name,
