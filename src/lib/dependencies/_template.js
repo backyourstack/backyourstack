e2635a871d08bb98e7b9e8fb41827ee9e471792b
Marc-Andre Lafortune <github@marc-andre.ca>
2018-08-24 17:26:18 -0400

    Add base template with doc

---
+++ src/lib/dependencies/_template.js
@@ -0,0 +1,33 @@
+// This is a template to add support for new package managers.
+
+const patterns = ['...']; // Patterns of filenames used by the package manager.
+                          // Supports '*' only at the beginning or end of a pattern.
+
+const searchAllRepo = false; // Flag for package managers with files not typically at the root level
+
+/**
+ * Computes the list of dependencies for the given file.
+ * @param {object} file - Keys: text, matchedPattern, ...
+ * @returns {object} list of dependencies, grouped by type,
+ *   where keys are any of {core|dev|peer|engines} and
+ *   values are [dependencyName, ...] or {dependencyName: , ...}
+ */
+function dependencies (/* file */) {
+  return { core: [] };
+}
+
+/**
+ * Computes the project name for the given file.
+ * @param {object} file - Keys: text, matchedPattern, ...
+ * @returns {String|null}
+ */
+function detectProjectName (/* file */) {
+  return null;
+}
+
+export {
+  patterns,
+  searchAllRepo,
+  dependencies,
+  detectProjectName,
+};
