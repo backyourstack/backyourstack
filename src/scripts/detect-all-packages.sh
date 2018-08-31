d866985fb857753cc0ec770ef82e09935e6cd964
François Hodierne <francois@hodierne.net>
2018-08-31 18:37:55 +0200

    scripts: detect-packages

---
+++ src/scripts/detect-all-packages.sh
@@ -0,0 +1,8 @@
+SCRIPTS_PATH=./src/scripts
+
+babel-node $SCRIPTS_PATH/detect-composer-packages.js
+babel-node $SCRIPTS_PATH/detect-gem-packages.js
+babel-node $SCRIPTS_PATH/detect-go-packages.js
+babel-node $SCRIPTS_PATH/detect-npm-packages.js
+babel-node $SCRIPTS_PATH/detect-nuget-packages.js
+babel-node $SCRIPTS_PATH/detect-pypi-packages.js
