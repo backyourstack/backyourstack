ac9ffbf0a526cc01a51c8d7e68781e904819eada
François Hodierne <francois@hodierne.net>
2018-10-09 09:02:02 +0200

    style: apply prettier

--- src/lib/dependencies/composer.js
+++ src/lib/dependencies/composer.js
@@ -1,26 +1,22 @@
 const patterns = ['composer.json'];
 
-function dependencyObject (composerJson) {
+function dependencyObject(composerJson) {
   return {
     core: Object.keys(composerJson.require || {}),
     dev: Object.keys(composerJson['require-dev'] || {}),
   };
 }
 
-function json (file) {
-  return file.json = file.json || JSON.parse(file.text);
+function json(file) {
+  return (file.json = file.json || JSON.parse(file.text));
 }
 
-function dependencies (file) {
+function dependencies(file) {
   return dependencyObject(json(file));
 }
 
-function detectProjectName (file) {
+function detectProjectName(file) {
   return json(file).name;
 }
 
-export {
-  patterns,
-  dependencies,
-  detectProjectName,
-};
+export { patterns, dependencies, detectProjectName };
--- src/lib/dependencies/npm.js
+++ src/lib/dependencies/npm.js
@@ -1,28 +1,24 @@
 const patterns = ['package.json'];
 
-function dependencyObject (packageJson) {
+function dependencyObject(packageJson) {
   return {
-    core:    Object.keys(packageJson.dependencies || {}),
-    peer:    Object.keys(packageJson.peerDependencies || {}),
-    dev:     Object.keys(packageJson.devDependencies || {}),
+    core: Object.keys(packageJson.dependencies || {}),
+    peer: Object.keys(packageJson.peerDependencies || {}),
+    dev: Object.keys(packageJson.devDependencies || {}),
     engines: Object.keys(packageJson.engines || {}),
   };
 }
 
-function json (file) {
-  return file.json = file.json || JSON.parse(file.text);
+function json(file) {
+  return (file.json = file.json || JSON.parse(file.text));
 }
 
-function dependencies (file) {
+function dependencies(file) {
   return dependencyObject(json(file));
 }
 
-function detectProjectName (file) {
+function detectProjectName(file) {
   return json(file).name;
 }
 
-export {
-  patterns,
-  dependencies,
-  detectProjectName,
-};
+export { patterns, dependencies, detectProjectName };
