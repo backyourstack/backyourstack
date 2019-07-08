d147451e073395caa99623fa3e2115aa34f2702d
flickz <omoyajowo2015@gmail.com>
2019-07-08 10:19:17 +0100

    feat(saveFile): add aws-sdk s3 upload api method to lib

---
+++ src/lib/awsS3.js
@@ -0,0 +1,34 @@
+import { S3 } from 'aws-sdk';
+import uuidv1 from 'uuid/v1';
+
+const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  apiVersion: '2006-03-01',
+});
+
+const Bucket = 'backyourstack-test';
+
+export const uploadFiles = files => {
+  const count = Object.keys(files).length;
+  const folder = `Project-${uuidv1()}`;
+  const promises = [];
+  Object.entries(files).map(([id, file]) => {
+    const filename = [uuidv1(), '.json'].join('');
+    let key;
+    if (count > 1) {
+      key = `${folder}/${filename}`;
+    } else {
+      key = filename;
+    }
+    const uploadParam = {
+      Bucket,
+      Key: key,
+      Body: JSON.stringify(file.json),
+      ACL: 'public-read',
+      ContentType: 'application/json',
+    };
+    promises.push(s3.upload(uploadParam).promise());
+  });
+  return Promise.all(promises);
+};
