8d8be13ec6038d3f9fd4338de17a429a96201742
flickz <omoyajowo2015@gmail.com>
2019-07-17 15:34:49 +0100

    feat(saveFile): remove modal and update Back My Stack button display in files page

--- src/components/BackMyStack.js
+++ src/components/BackMyStack.js
@@ -6,12 +6,42 @@ const BackMyStack = ({ onClickBackMyStack }) => {
       <style jsx>
         {`
           .backMyStackWrapper {
-            margin: 10px;
+            margin-bottom: 20px;
+            max-width: 90%;
+            display: flex;
+            align-items: center;
+            border: 1px solid #c1c6cc;
+            border-radius: 15px;
+            padding: 20px 40px;
+            justify-content: space-between;
+          }
+          .backMyStackInfoWrapper {
+            width: 60%;
+          }
+          .backMyStackInfoWrapper h2 {
+            color: #8f8f8f;
+          }
+          .backMyStackInfoWrapper p {
+            font-size: 1.3rem;
+          }
+          .backMyStackBtn {
+            height: 50px;
           }
         `}
       </style>
       <div className="backMyStackWrapper">
-        <button className="bigButton" onClick={onClickBackMyStack}>
+        <div className="backMyStackInfoWrapper">
+          <h2>NEW: Automatically Back Your Stack every month</h2>
+          <p>
+            With our monthly plan available from Open Collective, commit a fixed
+            amount every month and the money will be automatically dispatched to
+            your dependencies!
+          </p>
+        </div>
+        <button
+          className="bigButton backMyStackBtn"
+          onClick={onClickBackMyStack}
+        >
           Back My Stack
         </button>
       </div>
