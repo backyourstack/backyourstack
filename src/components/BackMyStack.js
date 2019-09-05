e48e163d480be97e313741694f1a347bfb1c3a88
flickz <omoyajowo2015@gmail.com>
2019-09-05 10:41:37 +0100

    refactor(file-design): add backmystack banner to BackMyStack component

--- src/components/BackMyStack.js
+++ src/components/BackMyStack.js
@@ -7,43 +7,87 @@ const BackMyStack = ({ onClickBackMyStack }) => {
         {`
           .backMyStackWrapper {
             margin-bottom: 20px;
-            max-width: 90%;
-            display: flex;
-            align-items: center;
-            border: 1px solid #c1c6cc;
+            width: 100%;
             border-radius: 15px;
-            padding: 20px 40px;
-            justify-content: space-between;
+            padding: 20px;
+            box-sizing: border-box;
+            background: url('/static/img/backmystack-banner.png');
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
           }
           .backMyStackInfoWrapper {
             width: 60%;
+            color: #fff;
+          }
+          .newText {
+            color: #ffc94d;
+            font-weight: 600;
+            font-size: 20px;
+            line-height: 20px;
+            margin-bottom: 20px;
           }
           .backMyStackInfoWrapper h2 {
-            color: #8f8f8f;
+            margin: 0;
+            font-weight: 900;
+            font-size: 32px;
+            line-height: 36px;
+            letter-spacing: -0.4px;
           }
           .backMyStackInfoWrapper p {
-            font-size: 1.3rem;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 24px;
           }
           .backMyStackBtn {
-            height: 50px;
-            cursor: pointer;
+            background: #2c2c85;
+            font-size: 14px;
+            line-height: 21px;
+          }
+          @media screen and (max-width: 768px),
+            @media screen and (max-width: 500px) {
+            .backMyStackWrapper {
+              background: url('/static/img/mobile-backmystack-banner.png');
+              background-size: 100% 100%;
+              background-repeat: no-repeat;
+              width: 100%;
+            }
+            .backMyStackInfoWrapper {
+              width: 78%;
+            }
+            .backMyStackInfoWrapper h2 {
+              margin: 0;
+              font-weight: bold;
+              font-size: 24px;
+              line-height: 28px;
+              letter-spacing: -0.4px;
+            }
+            .backMyStackInfoWrapper p {
+              font-size: 14px;
+              line-height: 22px;
+              letter-spacing: -0.2px;
+              font-weight: 300;
+            }
+            .newText {
+              margin-bottom: 20px;
+            }
           }
         `}
       </style>
       <div className="backMyStackWrapper">
         <div className="backMyStackInfoWrapper">
-          <h2>NEW: Automatically Back Your Stack every month</h2>
+          <h3 className="newText">New!</h3>
+          <h2>Automatically back your stack every month</h2>
           <p>
             Set up a monthly subscription and we will automatically distribute
             the funds among your dependencies on Open Collective!
           </p>
+          <button
+            className="bigButton backMyStackBtn"
+            onClick={onClickBackMyStack}
+          >
+            Back My Stack
+          </button>
         </div>
-        <button
-          className="bigButton backMyStackBtn"
-          onClick={onClickBackMyStack}
-        >
-          Back My Stack
-        </button>
       </div>
     </Fragment>
   );
