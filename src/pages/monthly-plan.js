9e57c06e865a48288f2f0165acec85b8ceb2cdaa
flickz <omoyajowo2015@gmail.com>
2019-09-10 08:58:33 +0100

    refactor(monthlyplan-design): redesign monthly-plan page

--- src/pages/monthly-plan.js
+++ src/pages/monthly-plan.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import NumberFormat from 'react-number-format';
 import { get } from 'lodash';
 
 import { fetchJson } from '../lib/fetch';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import UpArrow from '../static/img/up-arrow.svg';
+import DownArrow from '../static/img/down-arrow.svg';
 
 const EXAMPLE_VALUE = 200;
 const EXAMPLE_FEES = 0.15;
@@ -14,6 +18,65 @@ const getFilesData = sessionFiles =>
   process.env.IS_CLIENT
     ? fetchJson('/data/getFilesData')
     : import('../lib/data').then(m => m.getFilesData(sessionFiles));
+
+const suggestedAmounts = [
+  {
+    id: 1,
+    employeeRange: '100-200',
+    totalAmount: 2500,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 2,
+    employeeRange: '200-500',
+    totalAmount: 5000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 3,
+    employeeRange: '220-530',
+    totalAmount: 7000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 4,
+    employeeRange: '420-530',
+    totalAmount: 8000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 5,
+    employeeRange: '420-530',
+    totalAmount: 9000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 6,
+    employeeRange: '420-530',
+    totalAmount: 9000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+  {
+    id: 7,
+    employeeRange: '420-530',
+    totalAmount: 9000,
+    frequency: 'mo',
+    currency: 'USD',
+    currencySymbol: '$',
+  },
+];
 
 export default class MonthlyPlan extends React.Component {
   static async getInitialProps({ req, query }) {
@@ -45,6 +108,16 @@ export default class MonthlyPlan extends React.Component {
     recommendations: PropTypes.array,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAmount: {},
+      customAmount: '',
+      mobileToggleExpanded: true,
+      showCustomAmount: true,
+    };
+  }
+
   getContributionUrl = () => {
     // Get the key url of the file
     const { baseUrl, uuid } = this.props;
@@ -58,8 +131,256 @@ export default class MonthlyPlan extends React.Component {
     }
   };
 
+  handleAmountChange = event => {
+    const customAmount = event.target.value;
+    this.setState({
+      customAmount,
+    });
+  };
+
+  handleOnAmountSelect = selectedAmount => {
+    this.setState({
+      selectedAmount,
+    });
+  };
+
+  hanldeAmountSuggestionToggle = () => {
+    const { mobileToggleExpanded } = this.state;
+    this.setState({
+      mobileToggleExpanded: !mobileToggleExpanded,
+    });
+  };
+
+  getTotalAmount() {
+    const { selectedAmount, customAmount } = this.state;
+    if (selectedAmount && selectedAmount.id === 'custom' && customAmount) {
+      return customAmount;
+    } else if (selectedAmount && selectedAmount.totalAmount) {
+      return selectedAmount.totalAmount;
+    } else {
+      return 0;
+    }
+  }
+
+  renderFormattedAmount(amount, currencySymbol) {
+    return (
+      <NumberFormat
+        value={amount}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={currencySymbol}
+      />
+    );
+  }
+
+  renderSuggestedAmount() {
+    const { selectedAmount, customAmount, mobileToggleExpanded } = this.state;
+    let suggestedAmountsToShow;
+
+    // For mobile view
+    if (mobileToggleExpanded) {
+      suggestedAmountsToShow = suggestedAmounts;
+    } else {
+      // Only render one of the suggested amounts in mobile view
+      // if `mobileToggleExpanded` is false
+      suggestedAmountsToShow = [suggestedAmounts[0]];
+    }
+
+    return (
+      <Fragment>
+        <style>
+          {`
+              .amountList {
+                display: flex;
+                justify-content: center;
+                margin-bottom: 20px;
+                width: 80%;
+              }
+              .amountCard {
+                width: 250px;
+                min-height: 80px;
+                margin-right: 20px;
+                border: 1px solid #6F5AFA;
+                border-radius: 6px;
+                background: #fff;
+                text-align: center;
+                box-sizing: border-box;
+                padding: 5px;
+                cursor: pointer;
+                color: #141414;
+              }
+              .selectedAmountCard,
+              .amountCard:hover:not(.mobileSuggestedAmountToggle) {
+                border: 1px solid #6F5AFA;
+                background: #6F5AFA;
+                color: #fff;
+              }
+              .amountCard p {
+                margin: 0;
+              }
+              .customAmountText,
+              .employeeRange {
+                font-weight: bold;
+                font-size: 14px;
+                line-height: 22px;
+                letter-spacing: -0.2px;
+                line-height: 22px;
+              }
+              .employeeText {
+                font-weight: 600;
+                font-size: 12px;
+                line-height: 18px;
+                color: #9D9FA3;
+              }
+              .amountFig {
+                font-weight: bold;
+                font-size: 16px;
+                line-height: 24px;
+              }
+              .currrencyAndFreq {
+                font-weight: 400;
+                font-size: 11px;
+                line-height: 18px;
+                color: #76777A;
+              }
+              .customAmountCard {
+                padding: 10px;
+              }
+              .customAmountInput {
+                height: 40px;
+                border: 1px solid #C4C7CC;
+                box-shadow: inset 0px 2px 2px rgba(20, 20, 20, 0.08);
+                border-radius: 8px;
+                width: 130px;
+                display: flex;
+                align-items: center;
+                background: #fff;
+              }
+              .amountInput {
+                font-size: 14px;
+                line-height: 24px;
+                width: 80%;
+                border: none;
+                outline: none;
+                padding: 5px;
+              }
+              .amountInput::placeholder {
+                color: #9D9FA3;
+                font-size: 12px;
+              }
+              .prepend {
+                width: 20%;
+                background: #F5F7FA;
+                font-size: 14px;
+                line-height: 22px;
+                height: 99%;
+                border: none;
+                display: flex;
+                align-items: center;
+                color: #C0C5CC;
+                justify-content: space-around;
+                border-top-left-radius: 8px;
+                border-bottom-left-radius: 8px;
+              }
+              .mobileSuggestedAmountToggle {
+                display: none;
+              }
+              @media screen and (max-width: 640px) {
+                .customAmountCard {
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  justify-content: center;
+                }
+                .amountList {
+                  flex-direction: column;
+                }
+                .amountCard:not(.mobileSuggestedAmountToggle) {
+                  border-bottom: none;
+                  border-bottom-left-radius: 0;
+                  border-bottom-right-radius: 0;
+                }
+                .amountCard:not(:first-child) {
+                  border-top-left-radius: 0;
+                  border-top-right-radius: 0;
+                }
+                .mobileSuggestedAmountToggle {
+                  display: flex;
+                  min-height: 33px;
+                  align-items: center;
+                  justify-content: center;
+                }
+              }
+          `}
+        </style>
+        <div className="amountList">
+          {suggestedAmountsToShow.map(suggestedAmount => {
+            const selectedAmountCard = selectedAmount
+              ? selectedAmount.id === suggestedAmount.id
+              : false;
+            return (
+              <div
+                key={suggestedAmount.id}
+                className={classnames('amountCard', { selectedAmountCard })}
+                onClick={() => this.handleOnAmountSelect(suggestedAmount)}
+              >
+                <p className="employeeRange">{suggestedAmount.employeeRange}</p>
+                <p className="employeeText">Employee</p>
+                <div className="amountFigWrapper">
+                  <span className="amountFig">
+                    {this.renderFormattedAmount(
+                      suggestedAmount.totalAmount,
+                      suggestedAmount.currencySymbol,
+                    )}
+                  </span>{' '}
+                  <span className="currencyAndFreq">
+                    {suggestedAmount.currency}/{suggestedAmount.frequency}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+          <div
+            className={classnames('amountCard', 'customAmountCard', {
+              selectedAmountCard: selectedAmount.id === 'custom',
+            })}
+            onClick={() => this.handleOnAmountSelect({ id: 'custom' })}
+          >
+            <p className="customAmountText">Custom Amount</p>
+            <div className="customAmountInput">
+              <div className="prepend">$</div>
+              <input
+                type="number"
+                className="amountInput"
+                name="totalAmount"
+                placeholder="0.00 USD / mo"
+                value={customAmount}
+                onChange={this.handleAmountChange}
+              />
+            </div>
+          </div>
+          <div
+            className="amountCard mobileSuggestedAmountToggle"
+            onClick={this.hanldeAmountSuggestionToggle}
+          >
+            {mobileToggleExpanded ? (
+              <div className="arrowIconWrapper">
+                <UpArrow />
+              </div>
+            ) : (
+              <div className="arrowIconWrapper">
+                <DownArrow />
+              </div>
+            )}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+
   render() {
     const { loggedInUser, recommendations } = this.props;
+    const totalAmount = this.getTotalAmount();
 
     const opencollectiveRecommendations = recommendations
       .filter(r => r.opencollective)
@@ -75,12 +396,54 @@ export default class MonthlyPlan extends React.Component {
         <div className="Page BackMyStackPage">
           <style jsx>
             {`
+              .mainWrapper {
+                background: url(/static/img/background-colors.svg) no-repeat;
+                margin: 0;
+                padding: 20px;
+              }
+              .contentWrapper {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                wdith: 100%;
+                box-sizing: border-box;
+              }
               .content {
                 width: 540px;
-                border: 1px solid #dcdcdd;
                 border-radius: 12px;
-                margin: 50px auto;
+                margin-top: 15px;
                 padding: 20px 30px;
+                color: #141414;
+              }
+              .content h1 {
+                font-size: 32px;
+                line-height: 36px;
+                letter-spacing: -0.4px;
+              }
+              .content h4 {
+                font-size: 20px;
+                line-height: 22px;
+                letter-spacing: -0.2px;
+              }
+              .content p {
+                font-size: 14px;
+                line-height: 22px;
+                letter-spacing: -0.2px;
+              }
+              .content h5 {
+                font-size: 16px;
+                line-height: 24px;
+              }
+              .amountTableWrapper {
+                background: #fff;
+                border: 1px solid rgba(24, 26, 31, 0.1);
+                border-radius: 8px;
+                color: #76777a;
+                padding: 20px;
+              }
+              .tableDescription {
+                font-size: 14px;
+                font-weight: 400;
               }
               table {
                 width: 100%;
@@ -88,72 +451,127 @@ export default class MonthlyPlan extends React.Component {
               }
               table th,
               table td {
-                border: 1px solid #c1c6cc;
+                border-bottom: 1px solid #dcdee0;
                 padding: 0.5em;
                 white-space: nowrap;
-                font-size: 12px;
-                color: #121314;
+                font-size: 14px;
+              }
+              .checkbox {
+                border: 1px solid #c4c7cc;
+                box-shadow: inset 0px 2px 2px rgba(20, 20, 20, 0.08);
+                border-radius: 3px;
+              }
+              table tr th:nth-child(2),
+              table tr td:nth-child(2) {
+                text-align: right;
+              }
+              table tr th:nth-child(1),
+              table tr td:nth-child(1) {
+                text-align: left;
+              }
+              table th {
+                text-transform: uppercase;
+                font-weight: bold;
+                font-size: 10px;
               }
               .continueButton {
                 width: 200px;
                 margin: 50px auto 20px;
               }
+              .notice-p {
+                font-size: 12px;
+                font-weight: 300;
+              }
+              .continue-p {
+                font-size: 12px;
+                text-align: center;
+              }
               @media screen and (max-width: 640px) {
+                .mainWrapper {
+                  background: url(/static/img/mobile-background-colors.svg)
+                    no-repeat;
+                  background-size: 100%;
+                }
                 .content {
-                  width: auto;
-                  padding: 20px 15px;
-                  margin: 50px 20px;
+                  width: 90%;
+                }
+                .amountTableWrapper {
+                  margin: 10px 50px;
                 }
               }
             `}
           </style>
           <Header loggedInUser={loggedInUser} login={false} brandAlign="auto" />
-          <div className="content">
-            <p>Congratulations, you&apos;re about to Back Your Stack!</p>
-            <p>
-              You&apos;ll make a single monthly payment and we&apos;ll
-              distribute the funds to your detected dependencies below. If new
-              dependencies join in the future, they will be detected and added
-              the following month. Your subscription will not change, we&apos;ll
-              distribute it amongst more dependencies You can always update the
-              amount.
-            </p>
-            <p>
-              <strong>Example</strong>: you decide to give ${EXAMPLE_VALUE}{' '}
-              within the monthly plan.
-            </p>
-            <table>
-              <tr>
-                <th>Collective</th>
-                <th>Amount</th>
-              </tr>
-              {recommendations
-                .filter(r => r.opencollective)
-                .filter(r => r.opencollective.pledge !== true)
-                .map(recommendation => (
-                  <tr key={recommendation.name}>
-                    <td>
-                      <a
-                        href={`${process.env.OPENCOLLECTIVE_BASE_URL}/${recommendation.opencollective.slug}`}
-                      >
-                        {recommendation.opencollective.name}
-                      </a>
-                    </td>
-                    <td>
-                      ${singleValue} <sup>*</sup>
-                    </td>
+          <div className="mainWrapper">
+            <div className="contentWrapper">
+              <div className="content">
+                <h1>Congratulations 🎉</h1>
+                <h4>You&apos;re about to Back Your Stack!</h4>
+                <p>
+                  You&apos;ll make a single monthly payment and we&apos;ll
+                  distribute the funds to your detected dependencies below. If
+                  new dependencies join in the future, they will be detected and
+                  added the following month. Your subscription will not change,
+                  we&apos;ll distribute it amongst more dependencies You can
+                  always update the amount.
+                </p>
+                <h5>Back your stack membership</h5>
+                <p>
+                  Please select the membership you want to set as a monthly due,
+                  or use the custom field to select a different amount to
+                  contribute.
+                </p>
+              </div>
+              {this.renderSuggestedAmount()}
+              <div className="content amountTableWrapper">
+                <p className="tableDescription">
+                  You decided to give{' '}
+                  <strong>
+                    {this.renderFormattedAmount(totalAmount, '$')}
+                  </strong>{' '}
+                  within the <strong>monthly plan.</strong>
+                </p>
+                <table>
+                  <tr>
+                    <th>Collective</th>
+                    <th>Amount</th>
                   </tr>
-                ))}
-            </table>
-            <p>* May vary slightly depending on payment processor fees.</p>
-            <a
-              className="button bigButton continueButton"
-              href={`${this.getContributionUrl()}`}
-            >
-              Continue on Open Collective
-            </a>
+                  {recommendations
+                    .filter(r => r.opencollective)
+                    .filter(r => r.opencollective.pledge !== true)
+                    .map(recommendation => (
+                      <tr key={recommendation.name}>
+                        <td>
+                          <a
+                            href={`${process.env.OPENCOLLECTIVE_BASE_URL}/${recommendation.opencollective.slug}`}
+                          >
+                            {recommendation.opencollective.name}
+                          </a>
+                        </td>
+                        <td>
+                          ${singleValue} <sup>*</sup>
+                        </td>
+                      </tr>
+                    ))}
+                </table>
+                <p className="notice-p">
+                  *Final amount distributed may vary slightly depending on
+                  payment processor fees.
+                </p>
+                <a
+                  className="button bigButton continueButton"
+                  href={`${this.getContributionUrl()}`}
+                >
+                  Continue on Open Collective
+                </a>
+                <p className="continue-p">
+                  The rest of the back your stack process continues at{' '}
+                  <strong>opencollective.com</strong>
+                </p>
+              </div>
+            </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
       </Fragment>
     );
