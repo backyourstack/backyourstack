5a412c923c28ec3c86313b3f93c212abd989aeea
François Hodierne <francois@hodierne.net>
2021-08-22 11:10:30 +0200

    update to next 10.x

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,19 +9,13 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta
-            property="og:image"
-            content="https://backyourstack.com/static/img/logo-og-1.png"
-          />
-        </Head>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
